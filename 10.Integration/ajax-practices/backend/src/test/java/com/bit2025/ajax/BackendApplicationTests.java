43024a2bc456deaf855aa16bcdc5306aebb7a259
kimjuhan-95 <jhkim9596@gmail.com>
2025-11-04 17:41:58 +0900

    asd

---
+++ 10.Integration/ajax-practices/backend/src/test/java/com/bit2025/ajax/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit2025.ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
